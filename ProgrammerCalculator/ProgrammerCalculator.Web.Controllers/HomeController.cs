0d2370b164716c465450bda52c82baf72f14494b
Mitko Stoikov <mitkostoikov1988@gmail.com>
2017-10-04 22:49:21 +0300

    removed last operand not being assigned check after calculation is executed

--- ProgrammerCalculator/ProgrammerCalculator.Web.Controllers/HomeController.cs
+++ ProgrammerCalculator/ProgrammerCalculator.Web.Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Web.Mvc;
+using ProgrammerCalculator.Services.Infrastructure.Enumerations;
 using ProgrammerCalculator.Services.Infrastructure.Contracts;
 
 namespace ProgrammerCalculator.Web.Controllers
@@ -7,7 +8,6 @@ namespace ProgrammerCalculator.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ICalculator calculatorService;
-        private static bool inExpression = false;
         private static bool usedOperator = false;
 
         public HomeController(ICalculator calculatorService)
@@ -30,24 +30,18 @@ namespace ProgrammerCalculator.Web.Controllers
         // Validate number input to accept only certain values
         public ActionResult InputNumber(string numberValue, string calcInput)
         {
-            if (usedOperator)
+            if (calcInput == "0" || usedOperator)
             {
                 usedOperator = false;
-            }
-
-            if (calcInput == "0" || inExpression)
-            {
-                inExpression = false;
                 return this.Content(numberValue);
             }
-
 
             return this.Content(calcInput + numberValue);
         }
 
         public ActionResult Add(string calcInput, int fromBase)
         {
-            this.calculatorService.ChangeLastOperator(Services.Infrastructure.Enumerations.OperatorType.Addition);
+            this.calculatorService.ChangeLastOperator(OperatorType.Addition);
 
             if (usedOperator)
             {
@@ -57,17 +51,12 @@ namespace ProgrammerCalculator.Web.Controllers
             var result = this.calculatorService.Add(calcInput, fromBase);
             usedOperator = true;
 
-            if (this.calculatorService.LastOperator != Services.Infrastructure.Enumerations.OperatorType.None)
-            {
-                inExpression = true;
-            }
-
             return this.Content(result);
         }
 
         public ActionResult Subtract(string calcInput, int fromBase)
         {
-            this.calculatorService.ChangeLastOperator(Services.Infrastructure.Enumerations.OperatorType.Subtraction);
+            this.calculatorService.ChangeLastOperator(OperatorType.Subtraction);
 
             if (usedOperator)
             {
@@ -77,17 +66,12 @@ namespace ProgrammerCalculator.Web.Controllers
             var result = this.calculatorService.Substract(calcInput, fromBase);
             usedOperator = true;
 
-            if (this.calculatorService.LastOperator != Services.Infrastructure.Enumerations.OperatorType.None)
-            {
-                inExpression = true;
-            }
-
             return this.Content(result);
         }
 
         public ActionResult Multiply(string calcInput, int fromBase)
         {
-            this.calculatorService.ChangeLastOperator(Services.Infrastructure.Enumerations.OperatorType.Multiplication);
+            this.calculatorService.ChangeLastOperator(OperatorType.Multiplication);
 
             if (usedOperator)
             {
@@ -97,17 +81,12 @@ namespace ProgrammerCalculator.Web.Controllers
             var result = this.calculatorService.Multiply(calcInput, fromBase);
             usedOperator = true;
 
-            if (this.calculatorService.LastOperator != Services.Infrastructure.Enumerations.OperatorType.None)
-            {
-                inExpression = true;
-            }
-
             return this.Content(result);
         }
 
         public ActionResult Divide(string calcInput, int fromBase)
         {
-            this.calculatorService.ChangeLastOperator(Services.Infrastructure.Enumerations.OperatorType.Division);
+            this.calculatorService.ChangeLastOperator(OperatorType.Division);
 
             if (usedOperator)
             {
@@ -116,11 +95,6 @@ namespace ProgrammerCalculator.Web.Controllers
 
             var result = this.calculatorService.Divide(calcInput, fromBase);
             usedOperator = true;
-
-            if (this.calculatorService.LastOperator != Services.Infrastructure.Enumerations.OperatorType.None)
-            {
-                inExpression = true;
-            }
 
             return this.Content(result);
         }
